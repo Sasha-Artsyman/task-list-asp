0c77e2100f8ab9613b832a908973b6aec77b9fc8
Sasha Artsyman <65505084+Sasha-Artsyman@users.noreply.github.com>
2022-05-11 11:32:53 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,7 +1,9 @@
 // начальные данные
 List<AllTasks> tasks = new List<AllTasks>
 {
-    new() { Id = Guid.NewGuid().ToString(), Title = "Complete your to-do", Autor = "Admin", DateOfCreation = "01.01.2022 11:00:00", Description = "Complete your to-do list for today", Done = false}
+    new() { Id = Guid.NewGuid().ToString(), Title = "Complete your to-do", Autor = "Admin", DateOfCreation = "01.01.2022 11:00:00", Description = "Complete my to-do list for today", Done = true},
+    new() { Id = Guid.NewGuid().ToString(), Title = "End project", Autor = "Admin", DateOfCreation = "01.01.2022 11:21:01", Description = "End project at 18:00", Done = false},
+    new() { Id = Guid.NewGuid().ToString(), Title = "Meeting", Autor = "Admin", DateOfCreation = "01.01.2022 12:55:00", Description = "Meetingmeeting at 18:30", Done = false}
 };
 
 var builder = WebApplication.CreateBuilder();
@@ -11,6 +13,14 @@ app.UseDefaultFiles();
 app.UseStaticFiles();
 
 app.MapGet("/api/tasks", () => tasks);
+
+app.MapGet("/api/tasks/{id}", (string id) =>
+{
+    AllTasks? task = tasks.FirstOrDefault(t => t.Id == id);
+    if (task == null) return Results.NotFound(new { message = "Task not found!" });
+
+    return Results.Json(task);
+});
 
 app.Run();
 
