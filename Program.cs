6a3992372e6dd4561910d8cefc36b5f6cdc22ab2
Sasha Artsyman <65505084+Sasha-Artsyman@users.noreply.github.com>
2022-05-13 12:36:21 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,9 +1,9 @@
 // начальные данные
 List<AllTasks> tasks = new List<AllTasks>
 {
-    new() { Id = Guid.NewGuid().ToString(), Title = "Complete your to-do", Autor = "Admin", DateOfCreation = "01.01.2022 11:00:00", Description = "Complete my to-do list for today", Done = true},
-    new() { Id = Guid.NewGuid().ToString(), Title = "End project", Autor = "Admin", DateOfCreation = "01.01.2022 11:21:01", Description = "End project at 18:00", Done = false},
-    new() { Id = Guid.NewGuid().ToString(), Title = "Meeting", Autor = "Admin", DateOfCreation = "01.01.2022 12:55:00", Description = "Meetingmeeting at 18:30", Done = false}
+    new() { Id = Guid.NewGuid().ToString(), Title = "Complete your to-do", Autor = "Admin", DateOfCreation = "01.01.2022", Description = "Complete my to-do list for today", Done = true},
+    new() { Id = Guid.NewGuid().ToString(), Title = "End project", Autor = "Admin", DateOfCreation = "01.01.2022", Description = "End project at 18:00", Done = false},
+    new() { Id = Guid.NewGuid().ToString(), Title = "Meeting", Autor = "Admin", DateOfCreation = "01.01.2022", Description = "Meetingmeeting at 18:30", Done = false}
 };
 
 var builder = WebApplication.CreateBuilder();
@@ -32,6 +32,26 @@ app.MapDelete("/api/tasks/{id}", (string id) =>
     return Results.Json(task);
 });
 
+app.MapPost("/api/tasks", (AllTasks task) => {
+
+    task.Id = Guid.NewGuid().ToString();
+    tasks.Add(task);
+    return task;
+});
+
+app.MapPut("/api/tasks", (AllTasks taskData) => {
+
+    var task = tasks.FirstOrDefault(t => t.Id == taskData.Id);
+    if (task == null) return Results.NotFound(new { message = "Task not found!" });
+
+    task.Title = taskData.Title;
+    task.Autor = taskData.Autor;
+    task.DateOfCreation = taskData.DateOfCreation;
+    task.Description = taskData.Description;
+    task.Done = taskData.Done;
+    return Results.Json(task);
+});
+
 app.Run();
 
 public class AllTasks
@@ -39,7 +59,7 @@ public class AllTasks
     public string Id { get; set; } = "";
     public string Title { get; set; } = "";
     public string Autor { get; set; } = "";
-    public string DateOfCreation { get; set; } = DateTime.Now.ToString();
+    public string DateOfCreation { get; set; } = "";
     public string Description { get; set; } = "";
     public bool Done { get; set; } = false;
 }
